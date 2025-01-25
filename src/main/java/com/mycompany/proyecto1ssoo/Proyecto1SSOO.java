d665b0c7eb425fefbc23a0dc43ef58ccc27086d9
BeaCardozo <113352853+BeaCardozo@users.noreply.github.com>
2025-01-25 12:16:32 -0400

    Carga inicial

---
+++ src/main/java/com/mycompany/proyecto1ssoo/Proyecto1SSOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1ssoo;
+
+/**
+ *
+ * @author beacardozo
+ */
+public class Proyecto1SSOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
